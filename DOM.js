9cf6c317f52715df0f0f9487cfb698905a6ece9b
Anjali Kumari <113256438+anjali074@users.noreply.github.com>
2024-04-25 19:42:36 +0530

    Add files via upload
    All Dom(Documents oject model)in js problem basic proj

---
+++ DOM.js
@@ -0,0 +1,19 @@
+//console.log("Dom new");
+//alert("best code");
+//document.body.chilNodes[3].innerText="abcd";
+/*let heading=document.getElementById("heading");
+console.dir(heading);*/
+/*let headings=document.getElementByclassName("my-class");
+console.dir(headings);
+console.log(headings);*/
+/*let parahs=document.getElementsByTagName("p");
+console.dir(parahs);*/
+//Query-selectors
+
+/*let elements=document.querySelector(".my class");//1st element
+console.dir(firstEl);
+let allEl=document.querySelectorAll(".my classs");
+console.log(allEl);*/
+let div=document.querySelector(("div"));
+console.dir(div);
+
---
+++ dom1.js
@@ -0,0 +1,14 @@
+//pratices-1: create a H2 heading with text- "Hello javascript". Appened "from Apna college student to this text using js"
+
+/*let h2=document.querySelector("h2");
+console.dir(h2.innerText);
+h2.innerTex=h2.innerText+"from Apna college students";*/
+
+//pratice-2: create 3 divs with comon class anme-"box".Acess them & add same unique text to ech of them
+
+let divs=document.querySelectorAll(".box")
+console.log(".div0");
+//change propertie
+divs[0].innerText="new value 0";
+divs[1].innerText="new value 1";
+divs[2].innerText="new value 2";
